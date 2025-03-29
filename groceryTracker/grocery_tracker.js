49c40b97b66407169bf1f1a54f37bdbe6885ebe4
george <ezegeorgechukwuma@gmail.com>
2025-03-29 07:43:35 -0400

    minor space removal

--- groceryTracker/grocery_tracker.js
+++ groceryTracker/grocery_tracker.js
@@ -1,5 +1,3 @@
-
-
 // function to calculate totla grocery
 function groceryTracker(){
 let amount1 = parseFloat(document.getElementById('amount1').value);
@@ -8,6 +6,7 @@ let amount3 = parseFloat(document.getElementById('amount3').value);
 
 let sum = amount1 + amount2 + amount3;
 
+// transfers the text to p tag in the html f
 document.getElementById('result').innerText = `The total grocery is :$ ${sum}`;
 
 }
