7864f7ecea1858362794e419eb323b96c4d25128
george <ezegeorgechukwuma@gmail.com>
2025-03-29 07:40:59 -0400

    removed unnecessary declartion of variable at the top

--- groceryTracker/grocery_tracker.js
+++ groceryTracker/grocery_tracker.js
@@ -1,15 +1,14 @@
-let amount1;
-let amount2;
-let amount3;
 
 
+// function to calculate totla grocery
 function groceryTracker(){
-amount1 = parseFloat(document.getElementById('amount1').value);
-amount2 = parseFloat(document.getElementById('amount2').value);
-amount3 = parseFloat(document.getElementById('amount3').value);
+let amount1 = parseFloat(document.getElementById('amount1').value);
+let amount2 = parseFloat(document.getElementById('amount2').value);
+let amount3 = parseFloat(document.getElementById('amount3').value);
 
-sum = amount1 + amount2 + amount3;
+let sum = amount1 + amount2 + amount3;
 
 document.getElementById('result').innerText = `The total grocery is :$ ${sum}`;
 
 }
+
