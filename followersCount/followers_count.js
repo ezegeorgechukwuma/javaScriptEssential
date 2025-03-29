fffde5cfa6d1e8135010c5bee185d62de5d6c8f0
george <ezegeorgechukwuma@gmail.com>
2025-03-29 09:47:57 -0400

    add reset button

--- followersCount/followers_count.js
+++ followersCount/followers_count.js
@@ -4,6 +4,8 @@ function increaseCount() {
   count++; // Increment the count by 1
   displayCount(); // Display the count
   checkCountValue(); // Check count value and display messages
+  
+ 
 }
 
 function displayCount() {
@@ -13,7 +15,18 @@ document.getElementById('countDisplay').innerHTML=count; // Display the count in
 function checkCountValue() {
   if (count === 10) {
     alert("Your Instagram post gained 10 followers! Congratulations!");
-  } else if (count === 2) {
+  } else if (count === 20) {
     alert("Your Instagram post gained 20 followers! Keep it up!");
   }
 }
+
+function resetCount() {
+     // Reset the value of the element with the id 'countDisplay' to 0
+       count = 0;
+       displayCount();
+       alert("Followers count has been reset.")
+
+}
+
+ 
+ // reset followers to 0
